4d1d69763a5691968d1c0f297118670a53bf2a65
Wilker Mizael <107046187+wilkermizael@users.noreply.github.com>
2023-08-07 00:57:55 -0300

    refact ranking 1.0

--- src/controllers/infoUser.controller.js
+++ src/controllers/infoUser.controller.js
@@ -34,7 +34,7 @@ export  async function listUsers (req, res){
 export async function ranking( req,res){
     try{
         const ranking = await db.query(`
-        SELECT register.id, register.name, SUM(u."visitCount") as "visitCount", COUNT(u."userId") as linksCount
+        SELECT register.id, register.name, SUM(u."visitCount") as "visitCount", COUNT(u."userId") AS linksCount
         FROM register
         LEFT JOIN urlshort u ON register.id = u."userId"
         GROUP BY register.id, register.name
